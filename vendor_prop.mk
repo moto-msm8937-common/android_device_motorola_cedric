04cdda17b39a3d33d32dd2a1977c03b822d5f51f
Sohamlad7 <sohamlad7@gmail.com>
2019-12-21 21:23:56 +0530

    cedric: add ims props to vendor_prop.mk

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -9,3 +9,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.qfp=false
+
+# IMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.ims.volte=true \
+    ro.config.hw_volte_on=true
